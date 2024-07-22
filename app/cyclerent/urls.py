fdf3912f834bc0f1a8098ff29ac41d1cc53825f5
manylovmax <maximalist000@gmail.com>
2024-07-22 21:12:36 +0300

    viewsets bikes, rents, views bikes_for_rent, rent_the_bike, finish_the_rent

---
+++ app/cyclerent/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from rents import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'bikes', views.BikeViewSet)
+router.register(r'rents', views.RentViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user-create/', views.create_user, name='user_create'),
+    path('bikes-for-rent/', views.bikes_for_rent, name='bikes_for_rent'),
+    path('rent-the-bike/<int:pk>', views.rent_the_bike, name='rent_the_bike'),
+    path('finish-the-rent/<int:pk>', views.finish_the_rent, name='finish_the_rent'),
+]
---
+++ app/rents/views.py
@@ -0,0 +1,105 @@
+from datetime import datetime
+
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from rents.serializers import GroupSerializer, UserSerializer, BikeSerializer, RentSerializer
+from rents.models import Bike, Rent
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+@api_view(['POST'])
+def create_user(request):
+    serialized = UserSerializer(data=request.data)
+    if serialized.is_valid():
+        User.objects.create_user(
+            serialized.data['username'],
+            serialized.data['email'],
+            serialized.data['password']
+        )
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BikeViewSet(viewsets.ModelViewSet):
+    queryset = Bike.objects.all().order_by('brand')
+    serializer_class = BikeSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class RentViewSet(viewsets.ModelViewSet):
+    queryset = Rent.objects.all().order_by('start_at')
+    serializer_class = RentSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+@api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+def bikes_for_rent(request: Request):
+    serializer = BikeSerializer(Bike.objects.filter(is_rent=False).order_by('brand'), many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+def rent_the_bike(request: Request, pk: int):
+    try:
+        user = User.objects.get(pk=request.data['user_id'])
+    except User.DoesNotExist:
+        return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+    try:    
+        bike = Bike.objects.get(pk=pk)
+    except Bike.DoesNotExist:
+        return Response({'detail': 'Bike not found'}, status=status.HTTP_404_NOT_FOUND)
+    if bike.is_rent:
+        return Response({'detail': 'Bike is being rent'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    
+    bike.is_rent = True
+    bike.save()
+
+    rent = Rent(user=user, bike=bike, start_at=datetime.now())
+    rent.save()
+
+    return Response({}, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+def finish_the_rent(request: Request, pk: int):
+    try:
+        rent = Rent.objects.get(pk=pk)
+    except Rent.DoesNotExist:
+        return Response({'detail': 'Rent not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    rent.bike.is_rent = False
+    rent.bike.save()
+
+    rent.finish_at = datetime.now()
+    rent.save()
+
+    return Response({}, status=status.HTTP_200_OK)
+
