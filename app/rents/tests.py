95d758f37432a9f8fed4cfe36f96b538f9d2f2f2
manylovmax <maximalist000@gmail.com>
2024-07-25 17:00:24 +0300

    added tests for bikes_for_rent view

--- app/rents/tests.py
+++ app/rents/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 from rents.models import Bike, Rent
+from rents.serializers import BikeSerializer, RentSerializer
 
 
 class RegisterViewTest(TestCase):
@@ -30,3 +31,35 @@ class RegisterViewTest(TestCase):
         resp = self.client.post('/register/', data)
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(resp.json(), {"email": ["Обязательное поле."]})
+
+
+class bikes_for_rent_ViewTest(TestCase):
+
+    def setUp(self):
+        #Создание пользователя
+        User.objects.create_user(username='testuser1', password='12345')
+        Bike.objects.create(brand='Brand 1', model='Model 1', price_per_hour=100)
+        Bike.objects.create(brand='Brand 2', model='Model 2', price_per_hour=200)
+
+    def test_success(self):
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        resp = client.get('/bikes-for-rent/')
+        self.assertEqual(resp.status_code, 200)
+        expected_data = BikeSerializer(Bike.objects.filter(is_rent=False).all(), many=True).data
+        self.assertEqual(resp.data, expected_data)
+
+    def test_one_rent(self):
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        bike = Bike.objects.filter(brand='Brand 1').first()
+        bike.is_rent = True
+        bike.save()
+        resp = client.get('/bikes-for-rent/')
+        self.assertEqual(resp.status_code, 200)
+        expected_data = BikeSerializer(Bike.objects.filter(is_rent=False).all(), many=True).data
+        self.assertEqual(resp.data, expected_data)
