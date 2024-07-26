fbfc26fd5975679f854b4caedeee94eb4e8a736c
manylovmax <maximalist000@gmail.com>
2024-07-26 15:29:04 +0300

    added tests for rent-the-bike view

--- app/rents/test_views.py
+++ app/rents/test_views.py
@@ -1,5 +1,7 @@
 import pytest
 
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
@@ -54,10 +56,12 @@ class TestBikesForRentView:
     @pytest.mark.django_db
     def test_success(self):
         self.setUp()
+        # Авторизация
         client = APIClient()
         resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
         token = resp.data['access']
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
         resp = client.get('/bikes-for-rent/')
         assert resp.status_code == 200
         expected_data = BikeSerializer(Bike.objects.filter(is_rent=False).all(), many=True).data
@@ -66,10 +70,12 @@ class TestBikesForRentView:
     @pytest.mark.django_db
     def test_one_rent(self):
         self.setUp()
+        # Авторизация
         client = APIClient()
         resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
         token = resp.data['access']
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
         bike = Bike.objects.filter(brand='Brand 1').first()
         bike.is_rent = True
         bike.save()
@@ -77,3 +83,74 @@ class TestBikesForRentView:
         assert resp.status_code == 200
         expected_data = BikeSerializer(Bike.objects.filter(is_rent=False).all(), many=True).data
         assert resp.data == expected_data
+
+
+class TestRentTheBikeView:
+
+    def setUp(self):
+        user = User.objects.create_user(username='testuser1', password='12345')
+        Bike.objects.create(brand='Brand 1', model='Model 1', price_per_hour=100)
+        Bike.objects.create(brand='Brand 2', model='Model 2', price_per_hour=200)
+        return user
+    
+    @pytest.mark.django_db
+    def test_success(self):
+        self.setUp()
+        # Авторизация
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        bike = Bike.objects.first()
+        resp = client.post('/rent-the-bike/' + str(bike.id))
+        assert resp.status_code == 200
+        bike.refresh_from_db()
+        assert bike.is_rent == True
+        rent = Rent.objects.filter(user__username='testuser1').first()
+        assert rent.bike == bike
+
+    @pytest.mark.django_db
+    def test_bike_not_found(self):
+        self.setUp()
+        # Авторизация
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        resp = client.post('/rent-the-bike/' + '10')
+        assert resp.status_code == 404
+        assert resp.data == {'detail': 'Велосипед не найден'}
+
+    @pytest.mark.django_db
+    def test_bike_already_rent(self):
+        self.setUp()
+        # Авторизация
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        bike = Bike.objects.first()
+        bike.is_rent = True
+        bike.save()
+        resp = client.post('/rent-the-bike/' + str(bike.id))
+        assert resp.status_code == 200
+        assert resp.data == {'detail': 'Велосипед арендован'}
+
+    @pytest.mark.django_db
+    def test_cannot_rent_more_bikes(self):
+        user = self.setUp()
+        # Авторизация
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        bike = Bike.objects.first()
+        rent = Rent(user=user, bike=bike, start_at=datetime.now())
+        rent.save()
+        resp = client.post('/rent-the-bike/' + str(bike.id))
+        assert resp.status_code == 200
+        assert resp.data == {'detail': 'Нельзя арендовать больше велосипедов. Сначала оплатите аренду'}
