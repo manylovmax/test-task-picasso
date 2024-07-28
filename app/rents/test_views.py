ca6d2bb0af3084b57ed454d66f0f6ae923178618
manylovmax <maximalist000@gmail.com>
2024-07-28 21:11:11 +0300

    added TestFinishTheRentView

--- app/rents/test_views.py
+++ app/rents/test_views.py
@@ -1,5 +1,6 @@
 import pytest
 
+from unittest.mock import patch
 from datetime import datetime
 
 from django.contrib.auth.models import User
@@ -149,8 +150,67 @@ class TestRentTheBikeView:
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
 
         bike = Bike.objects.first()
-        rent = Rent(user=user, bike=bike, start_at=datetime.now())
-        rent.save()
+        Rent.objects.create(user=user, bike=bike, start_at=datetime.now())
         resp = client.post('/rent-the-bike/' + str(bike.id))
         assert resp.status_code == 200
         assert resp.data == {'detail': 'Нельзя арендовать больше велосипедов. Сначала оплатите аренду'}
+
+
+class TestFinishTheRentView:
+
+    def setUp(self):
+        user = User.objects.create_user(username='testuser1', password='12345')
+        bike1 = Bike.objects.create(brand='Brand 1', model='Model 1', price_per_hour=100)
+        Bike.objects.create(brand='Brand 2', model='Model 2', price_per_hour=200)
+        Rent.objects.create(bike=bike1, user=user, start_at=datetime.now())
+        return user
+    
+    @pytest.mark.django_db
+    def test_success(self):
+        user = self.setUp()
+        # Авторизация
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        rent = Rent.objects.first()
+        with patch('rents.views.calculate_price.delay') as mock_task:
+            resp = client.post('/finish-the-rent/' + str(rent.id))
+            assert mock_task.called
+            now = datetime.now()
+            assert resp.status_code == 200
+            rent = Rent.objects.filter(user=user).first()
+            assert rent.finish_at
+            assert rent.bike.is_rent == False
+    
+    @pytest.mark.django_db
+    def test_not_found(self):
+        user = self.setUp()
+        # Авторизация
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        rent = Rent.objects.first()
+        resp = client.post('/finish-the-rent/' + str(rent.id) + '1')
+        assert resp.status_code == 404
+
+
+    
+    @pytest.mark.django_db
+    def test_rent_already_finished(self):
+        user = self.setUp()
+        # Авторизация
+        client = APIClient()
+        resp = client.post('/api/token/', {'username': 'testuser1', 'password': '12345'}, format='json')
+        token = resp.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        rent = Rent.objects.first()
+        rent.finish_at = datetime.now()
+        rent.save()
+        resp = client.post('/finish-the-rent/' + str(rent.id))
+        assert resp.status_code == 200
+        assert resp.data == {'detail': 'Аренда уже завершена'}
