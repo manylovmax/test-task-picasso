1ce3164fc27229ad812b3cfa41de1aef92d22db7
manylovmax <maximalist000@gmail.com>
2024-08-04 20:02:13 +0300

    fix celery in docker-compose

--- app/cyclerent/settings.py
+++ app/cyclerent/settings.py
@@ -146,8 +146,8 @@ REST_FRAMEWORK = {
 # Celery Configuration Options
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
